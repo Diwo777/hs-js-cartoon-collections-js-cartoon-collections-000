9df7e3c717fe4df99eee1628728cbc3e5d83f474
Diego Zegarra <diego.zegarra79@icloud.com>
2020-03-11 20:27:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function dwarfRollCall(dwarves) {
-}
+
+function dwarfRollCall(dwarves){
+   var array =[]
+   for (var i=0;i<dwarves.length;i++){
+     array.push(`${i+1}. ${dwarves[i]} `)
+   }
+   return array.join("")
+ }
 
 function summonCaptainPlanet(planeteerCalls){
+  let uppercasePlaneteerCalls = [];
+  for(let i = 0; i < planeteerCalls.length; i++){
+    let punctuation = "!"
+    uppercasePlaneteerCalls.push(`${planeteerCalls[i]}${punctuation}`.toUpperCase())
+  }
+  return uppercasePlaneteerCalls
 }
 
 function longPlaneteerCalls(words) {
+if(words.length >= 4) {
+  return true
+} else (words.length < 4) 
+  return false
 }
 
 function findTheCheese (foods) {
+  for(var i=0;i<foods.length;i++){
+  if (foods[i]=="cheddar"
+  ||foods[i]=="gouda"
+  ||foods[i]=="camembert"
+  ){
+    return foods[i]
+  }
+}
+return "no cheese!"
 }
